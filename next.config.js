a4f9fd5bf671f39c2cd69a5e7f04a30600b5b7b3
Luke Stynes <lst89@uclive.ac.nz>
2024-08-18 16:33:34 +1200

    feat: redirect old routes

--- next.config.js
+++ next.config.js
@@ -16,6 +16,61 @@ const config = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/customer-reviews",
+        destination: "/client-reviews",
+        permanent: true,
+      },
+      {
+        source: "/contact-us",
+        destination: "/contact",
+        permanent: true,
+      },
+      {
+        source: "/homestay-in-christchurch",
+        destination: "/homestay",
+        permanent: true,
+      },
+      {
+        source: "/about-us",
+        destination: "/about",
+        permanent: true,
+      },
+      // Map old tours to new ones
+      {
+        source: "/tranzalpine-day-tour",
+        destination: "/tours/tranzalpine",
+        permanent: true,
+      },
+      {
+        source: "/christchurch-to-mt-cook-tour",
+        destination: "/tours/aoraki-mount-cook",
+        permanent: true,
+      },
+      {
+        source: "/christchurch-to-akaroa",
+        destination: "/tours/akaroa",
+        permanent: true,
+      },
+      {
+        source: "/kaikoura-whale-watch",
+        destination: "/tours/kaikoura-whale-watch",
+        permanent: true,
+      },
+      {
+        source: "/hanmer-springs-day-tour",
+        destination: "/tours/hanmer-springs",
+        permanent: true,
+      },
+      {
+        source: "/garden-tours-2",
+        destination: "/tours/christchurch-garden",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default config;
